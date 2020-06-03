6fe3235a6ba1c7c50f8583a4698603c5d1a62bb1
uncleurdnot <qminora@Gmail.com>
2020-06-03 00:13:51 -0700

    Update to dev branch

--- login.py
+++ login.py
@@ -1,5 +1,9 @@
 import hashlib
 import json
+import re
+import smtplib
+
+Username = ""
 
 def pwhasher(i):
   u = str.encode(i)
@@ -10,13 +14,24 @@ def pwhasher(i):
   #Generate Hash
   return pw.hexdigest()
 
+def isvalidpw(pw):
+  reg = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"
+  # compiling regex 
+  pat = re.compile(reg) 
+  # searching regex
+  return re.compile(reg)
+
+def isvalidemail(email):
+  reg = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+  # compiling regex 
+  pat = re.compile(reg) 
+  # searching regex                  
+  return re.search(pat, email)
+
 def create_user(u, pw, email): 
   u = u.lower()
   email = email.lower()
   filename = 'Data/sys/users.JSON'
-  if "\"username\": \"" + u + "\"," in open(filename).read():
-    print("User already exists")
-    return
   # python object to be appended
   y = {"username": u, 
       "email": pwhasher(email), 
@@ -50,3 +65,53 @@ def login_validator(u, pw):
           return True
         else:
           return False
+
+def login():
+  filename = 'Data/sys/users.JSON'
+  while True:
+    print("Menu\n\t1:\tLogin\n\t2:\tSign Up\n\t3:\tForgot Password")
+    choice = input().lower()
+    if choice == "1" or choice == "login":
+      print("Enter Username")
+      u = input().lower()
+      print("Enter Password")
+      pw = input().lower()
+      if login_validator(u,pw):
+        Username = u
+        return True
+      else:
+        print("Invalid Login")
+    elif choice == "2" or choice == "sign Up":
+      while True:
+        print("Enter Email")
+        e = input().lower()
+        if not isvalidemail(e):
+          print("Invalid Email")
+          continue
+        if "\"email\": \"" + pwhasher(e) + "\"," in open(filename).read():
+          print("User already exists")
+          continue
+        print("Enter Username")
+        u = input().lower()
+        if "\"username\": \"" + u + "\"," in open(filename).read():
+          print("User already exists")
+          continue
+        print("Enter Password")
+        pw = input().lower()
+        if not isvalidpw(pw):
+          print("Invalid Password")
+          continue
+        create_user(u, pw, e)
+        break
+    elif choice == "3" or choice == "forgot password":
+      print("Enter Email")
+      e = input().lower()
+      if not isvalidemail(e):
+        print("Invalid Email")
+        continue
+      if "\"email\": \"" + pwhasher(e) + "\"," not in open(filename).read():
+        print("User not found")
+        continue
+        
+    else:
+      print("Invalid Input")
--- main.py
+++ main.py
@@ -1,50 +1,18 @@
 import os
-import login
 import json
+import login
+import shutil
+import random
+import pack
 
 #Delete this later, Need to set up usernames
-Username = "uncleurdnot"
+Username = login.Username
 lib = []
-
-#Grab packs from /Data
-#Packs should include a Characters.JSON & a Powers.JSON
-def list_packs():
-  l = []
-  for root, dirs, files in os.walk("./Data"):
-    for name in files:
-      l.append(name)
-  return l
  
-def get_fname(origin):
-  filename = "Data/"+ origin + ".JSON"
-  return filename
-
-# Adds a new object to a pack.
-def write_json(origin, name, cat): 
-  name = name.lower()
-  origin = origin.lower()
-  filename = get_fname(cat, origin)
-  # python object to be appended 
-  y = {"origin": origin, 
-      "name": name, 
-      "added by": Username,
-      "rating": 0
-      } 
-  with open(filename) as json_file: 
-    data = json.load(json_file)
-    temp = data[cat]
-    #Check to see if the Object has already been added before
-    if "\"name\":\"" + name + "\"," in open(filename).read():
-      print("Already exists")
-      return
-    #appending data to Characters 
-    temp.append(y)
-    with open(filename,'w') as f: 
-      json.dump(data, f, indent=4) 
 
 #Function to modify the rating of a given JSON object
 def rate(origin, name, cat, v):
-  filename = get_fname(cat, origin)
+  filename = pack.pack.get_fname(cat, origin)
   with open(filename) as json_file: 
     data = json.load(json_file) 
     temp = data[cat]
@@ -59,7 +27,7 @@ def rate(origin, name, cat, v):
 
 #Add Pack to Selection
 def add_pack(origin, ec):
-  filename = get_fname(origin)
+  filename = pack.get_fname(origin)
   #If the desired pack is not already in lib, add it
   if filename not in lib:
     lib.append(filename)
@@ -70,7 +38,7 @@ def add_pack(origin, ec):
 
 #Remove Pack from selection
 def rem_pack(origin, ec):
-  filename = get_fname(origin)
+  filename = pack.get_fname(origin)
   if filename in lib:
     lib.remove(lib.index(filename)+1)
     lib.remove(filename)
@@ -80,48 +48,81 @@ def rem_pack(origin, ec):
 def zipq(list):
   l1 = []
   l2 = []
+  i = 0
   for x in list:
-    if x%2 == 0:
-      l1.append(list[x])
-    elif x%2 == 1:
-      l2.append(list[x])
+    if i%2 == 0:
+      l1.append(x)
+    if i%2 == 1:
+     l2.append(x)
+    i += 1    
   return zip(l1, l2)
 
 #Import Seclected Packs
 def import_packs():
   flib = zipq(lib)
-  temp = ""
+  #print(flib)
+  temp = [[],[],[],[]]
   for filename, excl in flib:
     with open(filename) as json_file: 
-      data = json.load(json_file) 
-      temp += data['Characters']
-      temp += data['Powers']
-      temp += data['WinCons']
-      temp += data['Arenas'] 
-      return temp
+      data = json.load(json_file)
+      if excl[0] == "1":
+        temp[0].append(data['characters'])
+      if excl[1] == "1":
+        temp[1].append(data['powers'])
+      if excl[2] == "1":
+        temp[2].append(data['winconds'])
+      if excl[3] == "1":
+        temp[3].append(data['arenas'])
+  return temp
 
 def select_packs():
   while True:
-    print("Add, Remove, Finish?")
+    print(lib)
+    print("Add, Remove, Show, Finish?")
     choice = input().lower()
     if choice == "add":
       print("Enter Pack Name")
       o = input()
       #Exclusion codes are hex values that will later be converted to binary
       print("Enter exclusion code")
-      ec = format(bin(int(input(), 16)), '04b')
+      ec = int(input(), 16)
+      ec = '{:04b}'.format(ec)
       add_pack(o,ec)
     elif choice == "remove":
       print("Enter Pack Name")
       o = input()
       #Exclusion codes are hex values that will later be converted to binary
       print("Enter exclusion code")
-      ec = format(bin(int(input(), 16)), '04b')
+      ec = int(input(), 16)
+      ec = '{:04b}'.format(ec)
       rem_pack(o,ec)
-    elif choice == "Finish":
+    elif choice == "show":
+      print(pack.list_packs())
+    elif choice == "finish":
       break
     else:
       print("Invalid Entry")
-    import_packs
+  return import_packs()
 
+def mainmenu():
+  while True:
+    print("Menu:\n\t1:\tPlay\n\t2:\tCreate Pack\n\t3:\tSearch Pack\n\t4:\tSettings\n\t5:\tExit")
+    ch = input().lower()
+    if ch == "play" or ch == "1":
+      deck = select_packs()
+    elif ch == "create pack" or ch == "create" or ch == "2":
+      print("Input pack name:")
+      o = input()
+      pack.creat_pack(o)
+      continue
+    elif ch == "search pack" or ch == "search" or ch == "3":
+      continue
+    elif ch == "settings" or ch == "4":
+      break
+    elif ch == "exit" or ch == "5":
+      break
+    else:
+      print("Invalid Input")
 
+if login.login():
+  mainmenu()
